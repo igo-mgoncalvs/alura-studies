8c7b4f5dc50186dcb7086258d6dbec091701f8ef
Igor <igor2013mgoncalves@gmail.com>
2022-06-08 10:44:53 -0300

    Projeto desenvolvido na aula de TypeScript da Alura,

---
+++ src/components/pages/App.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { ITarefa } from '../../types/tarefa';
+import Cronometo from '../cronometro';
+import Formulario from '../formulario';
+import Lista from '../lista';
+import style from './App.module.scss'
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
+  function finalizarTarefa () {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas}/>
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa = {selecionaTarefa}
+        />
+      <Cronometo 
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+        />
+    </div>
+  );
+}
+
+export default App;
